ac8eb6eba6e55a1bab21f9f6b8b60b303c5b5dc1
Wanderwool <wanderwool2x@gmail.com>
2023-03-20 21:49:10 +0200

    Add operate function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var firstNumber;
+var secondNumber;
+var operator;
+
 const add = (a, b) => a+b;
 
 const substract = (a,b) => a-b;
@@ -5,3 +9,23 @@ const substract = (a,b) => a-b;
 const multiply = (a,b) => a*b;
 
 const divide = (a,b) => a/b;
+
+const operate = (firstNumber, secondNumber, operator) => {
+    switch(operator){
+        case '+':
+            result = add(firstNumber + secondNumber)
+            break
+        case '-':
+            result = substract(firstNumber + secondNumber)
+            break
+        case '*':
+            result = multiply(firstNumber + secondNumber)
+            break      
+        case '/':
+            result = divide(firstNumber + secondNumber)
+            break        
+            
+
+    }
+    return result
+}
